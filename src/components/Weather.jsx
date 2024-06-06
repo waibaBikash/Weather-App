b2b8df58d881e1e5e448ee2c1552ce8b63ce1b25
waibaBikash <kavrelibikash@gmail.com>
2024-06-06 22:43:27 +1000

    create a weather ui

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Weather.css';
+import search_icon from '../assets/search.png';
+import clear_icon from '../assets/clear.png';
+import cloud_icon from '../assets/cloud.png';
+import drizzle_icon from '../assets/drizzle.png';
+import rain_icon from '../assets/rain.png';
+import snow_icon from '../assets/snow.png';
+import wind_icon from '../assets/wind.png';
+import humidity_icon from '../assets/humidity.png';
 
 const Weather = () => {
   return (
     <div className='weather'>
       <div className="search-bar">
          <input type="text" placeholder='search' />
-         <img src="" alt="" />
+         <img src={search_icon} alt="" />
       </div>
+       <img src={clear_icon} alt=""  className='weather-icon'/>
+       <p>16°C</p>
+       <p>Sydney</p>
     </div>
   )
 }
