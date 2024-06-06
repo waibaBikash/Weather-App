6c6bca0125e1e05b9b87402e840e2d574418b2ec
waibaBikash <kavrelibikash@gmail.com>
2024-06-06 22:11:59 +1000

    create a weather page

---
+++ src/components/Weather.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Weather.css';
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+      <div className="search-bar">
+         <input type="text" placeholder='search' />
+         <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Weather;
